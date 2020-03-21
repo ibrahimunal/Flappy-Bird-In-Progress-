1439b200b67bc02539653c6932e5f1cf95757c99
ibrahim <ibrahimm.unal@gmail.com>
2020-03-21 12:05:30 +0300

    First Commit

---
+++ Assets/Camera.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+
+
+    void Start()
+    {
+   
+
+    }
+   void FixedUpdate()
+    {
+        transform.position += new Vector3(GetComponent<FBird>().speedForward*Time.deltaTime,0,0);
+    }
+
+
+
+    // Update is called once per frame
+     void Update()
+    {
+        
+    }
+     void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (transform.tag != ("block"))
+        {
+
+            float width = ((BoxCollider2D)collision).size.x;
+            collision.transform.position += new Vector3(width * 7, 0, 0);
+        }
+        else  { 
+
+            collision.transform.position += new Vector3(16f,0,0);
+
+        }
+    }
+
+}
+
---
+++ Assets/FBird.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FBird : MonoBehaviour
+{
+    public float force;
+    private bool isFlapping;
+    Vector3 birdMove = Vector3.zero;
+    public Vector3 flyingSpeed;
+    public  float speedForward;
+    void Start()
+    {
+    }
+    void FixedUpdate()
+    {
+
+        if (transform.position.y < 0) {
+            transform.rotation = Quaternion.Euler(0, 0, -45 * Time.deltaTime);
+
+        }
+
+        birdMove += new Vector3(speedForward, 0, 0);
+
+        birdMove += new Vector3(0, -force, 0);
+        if (isFlapping == true) {
+            isFlapping = false;
+            birdMove += flyingSpeed;
+
+        }
+            transform.position += birdMove *Time.deltaTime;
+        
+    }
+
+    // update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space)){
+            isFlapping = true;
+        }
+  
+
+
+    }
+}
+
+
